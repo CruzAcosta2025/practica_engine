5b95902907f99de91d770d662d8e1f5ac7248c45
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:21:16 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Git Lanza Nueva Versión con Mejora en la Gestión de Ramas",
+    "autor" => "Carlos Mendoza",
+    "resumen" => "La última actualización de Git introduce nuevas funciones para facilitar la gestión de ramas y mejorar la colaboración entre desarrolladores.",
 ];
 ?>
