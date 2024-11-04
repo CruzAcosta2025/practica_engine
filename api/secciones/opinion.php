5b0c197c15862381ec2d216a4cb6fdd75f296f54
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:23:59 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La Necesidad de un Nuevo Enfoque Educativo en Perú",
+    "autor" => "Clara Ruiz",
+    "resumen" => "En esta columna, se discute la importancia de reformar el sistema educativo peruano para adaptarse a las nuevas demandas del mercado laboral y las habilidades del futuro.",
 ];
 ?>
