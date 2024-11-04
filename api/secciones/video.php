c83224362ceb656daf7aa6d5f6d15996d0f925df
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:27:14 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Enfriaron a Makanaky",
+"autor" => "Goblinciano",
+"resumen" => "En el presente video se muestra imagenes reales del fallecido influencer Makanaky",
 ];
 ?>
