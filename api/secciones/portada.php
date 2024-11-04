b570fa381c0702826887e23a126c196e4ac722d4
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:24:30 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Tendencias",
-"autor" => "sistemas@uns",
-"resumen" => "Las ultimas tendencias 2024",
+    "titulo" => "Tendencias en Tecnología para 2024",
+    "autor" => "sistemas@uns",
+    "resumen" => "Exploramos las últimas tendencias en tecnología, desde inteligencia artificial hasta sostenibilidad en la industria.",
 ];
 ?>
