1fe3927e8c8c3b526dd3d87d9d8563bacc1fb684
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:22:27 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Gobierno Peruano Anuncia Nuevo Plan de Vacunación contra la COVID-19",
+    "autor" => "Ana Torres",
+    "resumen" => "El Ministerio de Salud presenta un plan para reforzar la vacunación, buscando inocular a un mayor porcentaje de la población ante la llegada de nuevas variantes.",
 ];
 ?>
