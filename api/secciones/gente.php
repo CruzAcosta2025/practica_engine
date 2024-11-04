1781ecfb654cb0727c9ede531351934d9711b7dd
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:20:24 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Famoso Chef Peruano Gana Premio Internacional de Gastronomía",
+    "autor" => "María Rodríguez",
+    "resumen" => "El chef Gastón Acurio recibe un prestigioso premio en reconocimiento a su contribución a la cocina peruana y su labor en la promoción de la gastronomía local.",
 ];
 ?>
