85bf538dfd08e9894d7ae418a5a390639ffbb50b
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:19:49 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+    "titulo" => "Nuevo Programa de Becas para Estudiantes de Tecnología en Perú",
+    "autor" => "sistemas@uns",
+    "resumen" => "El gobierno peruano lanza un programa de becas para estudiantes en carreras de tecnología, buscando aumentar la capacitación en habilidades digitales.",
 ];
 ?>
