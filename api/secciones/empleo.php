9a6c4965cecade2bde1cd4e6679ac10cd91d649d
Ruben Cruz <acostakaku420@gmail.com>
2024-11-04 10:19:22 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Perú Registra Aumento en la Tasa de Empleo Formal en 2024",
+    "autor" => "Luis Gómez",
+    "resumen" => "El Ministerio de Trabajo anunció un incremento del 5% en el empleo formal, impulsado por políticas de fomento laboral y recuperación económica.",
 ];
 ?>
